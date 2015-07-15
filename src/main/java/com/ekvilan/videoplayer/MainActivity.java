7303c73c60852278f2460ef71b6a55d56dc528be
Ekvi <dima_sh84@mail.ru>
2015-07-15 18:14:00 +0300

    upload project

---
+++ src/main/java/com/ekvilan/videoplayer/MainActivity.java
@@ -0,0 +1,119 @@
+package com.ekvilan.videoplayer;
+
+import android.app.Activity;
+import android.media.AudioManager;
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.PopupWindow;
+import android.widget.RelativeLayout;
+
+
+public class MainActivity extends Activity implements SurfaceHolder.Callback, MediaPlayer.OnPreparedListener {
+    private MediaPlayer mediaPlayer;
+    private SurfaceHolder vidHolder;
+    private SurfaceView vidSurface;
+    //String vidAddress = "https://archive.org/download/ksnn_compilation_master_the_internet/ksnn_compilation_master_the_internet_512kb.mp4";
+    //String vidAddress = "http://testapi.qix.sx/video/sky.mp4";
+    String vidAddress = "http://testapi.qix.sx/video/mamahohotala.mp4";
+
+    private boolean isShowTop = false;
+    //boolean click = false;
+
+    Button btnClick;
+    PopupWindow topWindow;
+    PopupWindow bottomWindow;
+    RelativeLayout layout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        showTopWindow();
+        showBottomWindow();
+        layout = (RelativeLayout)findViewById(R.id.mainLayout);
+
+        vidSurface = (SurfaceView) findViewById(R.id.surfView);
+        vidHolder = vidSurface.getHolder();
+        vidHolder.addCallback(this);
+
+        vidSurface.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (isShowTop) {
+                    topWindow.dismiss();
+                    bottomWindow.dismiss();
+                    isShowTop = false;
+                }
+                else {
+                    topWindow.showAtLocation(layout, Gravity.TOP, 0, 20);
+                    //topWindow.update(20, 20, 300, 80);
+                    bottomWindow.showAtLocation(layout, Gravity.BOTTOM, 0, 0);
+                    //bottomWindow.update(20, 20, 300, 80);
+                    isShowTop = true;
+                }
+            }
+        });
+
+
+
+
+    }
+
+
+
+    private void showTopWindow() {
+        LayoutInflater layoutInflater = (LayoutInflater) getBaseContext()
+                .getSystemService(LAYOUT_INFLATER_SERVICE);
+        View popupView = layoutInflater.inflate(R.layout.popup_top, null);
+        topWindow = new PopupWindow(popupView,
+                ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+    }
+
+    private void showBottomWindow() {
+        LayoutInflater layoutInflater = (LayoutInflater) getBaseContext()
+                .getSystemService(LAYOUT_INFLATER_SERVICE);
+        View popupView = layoutInflater.inflate(R.layout.popup_bottom, null);
+        /*bottomWindow = new PopupWindow(popupView,
+                ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);*/
+        bottomWindow = new PopupWindow(popupView,
+                ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+    }
+
+    @Override
+    public void surfaceChanged(SurfaceHolder arg0, int arg1, int arg2, int arg3) {
+        // TODO Auto-generated method stub
+    }
+
+    @Override
+    public void surfaceCreated(SurfaceHolder arg0) {
+        try {
+            mediaPlayer = new MediaPlayer();
+            mediaPlayer.setDisplay(vidHolder);
+            mediaPlayer.setDataSource(vidAddress);
+            mediaPlayer.prepare();
+            mediaPlayer.setOnPreparedListener(this);
+            mediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
+        }
+        catch(Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void surfaceDestroyed(SurfaceHolder arg0) {
+        // TODO Auto-generated method stub
+    }
+
+    @Override
+    public void onPrepared(MediaPlayer mp) {
+        mediaPlayer.start();
+    }
+}
